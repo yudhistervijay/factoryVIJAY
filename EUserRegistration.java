ec9b7c075b4e11a01f34d8ed4546e6da555d2f11
yudhistervijay <119295140+yudhistervijay@users.noreply.github.com>
2023-02-16 17:11:19 +0530

    Add files via upload

---
+++ EUserRegistration.java
@@ -0,0 +1,73 @@
+package com.factory.appraisal.vehiclesearchapp.persistence.model;
+//@Author: Yudhister vijay
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
+
+import javax.persistence.*;
+
+@Audited
+@AuditTable(value = "USER_REG_AUD" , schema = "FACTORY_AUD")
+@Entity
+@Table(name = "USER_REG",schema = "FACTORY_DB")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+@AttributeOverride(name = "userId", column = @Column(name = "USER_ID"))
+@AttributeOverride(name = "valid", column = @Column(name = "IS_ACTIVE"))
+@AttributeOverride(name = "createdBy", column = @Column(name = "CREATED_BY"))
+@AttributeOverride(name = "createdOn", column = @Column(name = "CREATED_ON"))
+@AttributeOverride(name = "modifiedBy", column = @Column(name = "MODIFIED_BY"))
+@AttributeOverride(name = "modifiedOn", column = @Column(name = "MODIFIED_ON"))
+public class EUserRegistration extends TransactionEntity{
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy= GenerationType.SEQUENCE ,generator = "user_reg_seq")
+    @SequenceGenerator(name = "user_reg_seq" , sequenceName = "USER_REG_SEQ")
+    private Long userId;
+    @Column(name = "APT_NUMBER")
+    private String apartmentNumber;
+    private String city;
+    private String email;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    private String password;
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
+    private String state;
+    @Column(name = "STREET_ADDRESS")
+    private String streetAddress;
+    @Column(name = "USER_NAME")
+    private String userName;
+    @Column(name = "ZIP_CODE")
+    private String zipCode;
+
+    @OneToOne(targetEntity = EConfigurationCodes.class, fetch = FetchType.LAZY , cascade = CascadeType.ALL)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    @JoinColumn(name = "ROLE_ID", referencedColumnName = "CODE_ID", nullable = false)
+    private EConfigurationCodes roleId;
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    @OneToOne(targetEntity = EDealerRegistration.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "DEALER_ID", referencedColumnName = "DEALER_ID",nullable = false)
+    @Column(name = "DEALER_ID")
+    private EDealerRegistration dealerId;
+
+
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    @OneToOne(mappedBy = "userId")
+    private EAppraiseVehicle appraiseVehicle;
+
+
+
+}
---
+++ UserRegistration.java
@@ -0,0 +1,63 @@
+package com.factory.appraisal.vehiclesearchapp.dto;
+//@Author:Yudhister vijay
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistration {
+
+    @Size(max = 10)
+    private String apartmentNumber;
+    @NotNull
+    @Size(max = 10)
+    private String city;
+    @NotNull
+    @Email(message = "invalid email id")
+    @Size(max = 50)
+    private String email;
+    @NotNull
+    @Size(max = 15)
+    private String firstName;
+    @NotNull
+    @Size(max = 15)
+    private String lastName;
+    @NotNull
+    @Size(max = 20)
+    private String password;
+    @NotNull
+    @Size(max = 13)
+    @Pattern(regexp = "^\\+1\\s\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$")
+    /* Some valid examples of phone numbers
+    +1 (555) 123-4567
+    +1 555 123-4567
+    +1 5551234567     */
+    private String phoneNumber;
+    @NotNull
+    @Size(max = 10)
+    private String state;
+    @NotNull
+    @Size(max = 50)
+    private String streetAddress;
+    @NotNull
+    @Size(max = 30)
+    private String userName;
+    @NotNull
+    @Size(max = 5)
+    private String zipCode;
+
+    private ConfigurationCodes roleId;
+
+    private DealerRegistration dealerId;
+
+}
